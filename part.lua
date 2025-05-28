f03d8d9656bcf62a426334ffb7f71375143567f1
nathan <000savior001@gmail.com>
2025-05-27 18:30:53 -0700

    update: parts fall from random location

--- part.lua
+++ part.lua
@@ -5,13 +5,14 @@ function M.spawnNew(partTypes, platforms)
     local randomType = partTypes[math.random(#partTypes)]
     local currentPlatformIndex = 1
     local platform = platforms[currentPlatformIndex]
+    local spawnX = math.random(150, 700)
 
     platform.canTilt = false
     platform.angle = 0
 
     local part = {
-        x = platform.x,
-        y = platform.y - 100,
+        x = spawnX,
+        y = platforms[1].y - 100,
         size = 20,
         vx = 0,
         vy = 100,
